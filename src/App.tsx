af94c41896e669acd1cd42ed04bfc97fbe4071f7
Ana Lívia <analiviamgarbin@gmail.com>
2022-03-04 22:28:24 -0300

    scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 export function App() {
   return (
-    <Home />
+    <NewRoom />
   );
 }
+
+export default App;
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,17 +1,10 @@
-import { useNavigate } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 import '../styles/auth.scss'
 import { Button } from '../components/Button';
 
-export function Home() {
-  const navigate = useNavigate();
-
-  function navigateToNewRoom() {
-    navigate('/rooms/news');
-  }
-
+export function NewRoom() {
   return (
     <div id="page-auth">
       <aside>
@@ -22,17 +15,14 @@ export function Home() {
       <main>
         <div className='main-content'>
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={navigateToNewRoom} className='create-room'>
-            <img src={googleIconImg} alt="Icon do Google" />
-            Crie sua sala com o Google
-          </button>
-          <div className='separator'>
-            ou entre em uma sala
-          </div>
+          <h2>Criar uma nova sala:</h2>
           <form>
-            <input type="text" placeholder='Digite o código da sala' />
-            <Button type="submit">Entrar na sala</Button>
+            <input type="text" placeholder='Nome da sala' />
+            <Button type="submit">Criar sala</Button>
           </form>
+          <p>
+            Quer entrar em uma sala existente? <a href='#'>Clique aqui</a>
+          </p>
         </div>
       </main>
     </div>
